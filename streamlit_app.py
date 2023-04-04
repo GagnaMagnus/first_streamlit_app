ee03e3bc7b4e872a42a061398366a273b67ff461
GagnaMagnus <71044789+MaGudmunds@users.noreply.github.com>
2023-04-04 15:25:02 +0000

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-if add_myfruit != '':
+if add_my_fruit != '':
   streamlit.write(f'Thanks for adding {add_my_fruit}')
